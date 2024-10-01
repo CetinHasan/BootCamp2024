8eabfd22de602fb92bdad8d3d68855d3322b08fe
cetin <cetin@TUF>
2024-10-01 13:31:11 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region test
+            Console.Write("test");
+
+            Console.Read();
+
+            #endregion
+        }
+    }
+}
